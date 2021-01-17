bbcaaea703c48eef22ffa709d629b7c812fe7d63
Bohdan <zin4enko.bod@gmail.com>
2021-01-17 13:25:58 +0200

    Add project

---
+++ PollManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PollManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
